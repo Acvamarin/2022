e875f918818419f1cb474f027a86b8cbcd17c81e
Acvamarin <makavchik.kirill@mail.ru>
2021-07-03 14:37:25 +0300

    123

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            import React from 'react'
+            
+            const index = () => {
+                return (
+                    <div>
+                        
+                    </div>
+                )
+            }
+            
+            export default index
+            
+        </div>
+    )
+}
+
+export default index
